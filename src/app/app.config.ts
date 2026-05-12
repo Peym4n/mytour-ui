e9747824f6c4cd12a38ebf31911d69c9f5203830
Peyman Aparviz <peyman.aparviz@gmail.com>
2026-05-13 01:43:51 +0200

    generated models and services based on OpenAPI docs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,15 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { provideApiConfiguration } from './api/generated/api-configuration';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
+    provideApiConfiguration(environment.apiUrl),
     providePrimeNG({
       theme: {
         preset: Aura
