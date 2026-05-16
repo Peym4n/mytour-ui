dbe4ed1924688a79f519366d82449e386cef3c5d
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-16 11:30:26 +0200

    update routing configuration to include tours and health paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,11 @@ import { Routes } from '@angular/router';
 import { HealthComponent } from './health';
 
 export const routes: Routes = [
-  { path: 'health', component: HealthComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'tours' },
+  {
+    path: 'tours',
+    loadChildren: () => import('./tours/tours.routes').then((m) => m.toursRoutes)
+  },
+  { path: 'health', component: HealthComponent },
+  { path: '**', redirectTo: 'tours' }
 ];
