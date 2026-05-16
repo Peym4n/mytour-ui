6cfdcc12b0d6459e8fa87c3b3f8efc26d539a290
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-16 12:00:50 +0200

    feat: add routes for creating and editing tours in the tour form

--- src/app/tours/tours.routes.ts
+++ src/app/tours/tours.routes.ts
@@ -2,6 +2,16 @@ import { Routes } from '@angular/router';
 
 export const toursRoutes: Routes = [
   {
+    path: 'new',
+    loadComponent: () =>
+      import('./tour-form/tour-form').then((m) => m.TourFormComponent)
+  },
+  {
+    path: ':tourId/edit',
+    loadComponent: () =>
+      import('./tour-form/tour-form').then((m) => m.TourFormComponent)
+  },
+  {
     path: ':tourId',
     loadComponent: () =>
       import('./tour-detail/tour-detail').then((m) => m.TourDetailComponent)
